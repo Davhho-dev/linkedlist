c2eb0039d4ee9cd14dd3ffeec78d26f0decd0316
Davhho-dev <Davhho@utexas.edu>
2023-04-08 10:45:22 -0500

    Revise code

--- linkedlist.js
+++ linkedlist.js
@@ -1,9 +1,25 @@
-import Node from "./node"
+import Node from "../linkedlist/node.js"
 
-const LinkedList = () => {
+function LinkedList() {
     let head = null;
     let count = 0;
-
+    
+    const append = (value) => {
+        count++;
+        const node = new Node (value);
+        if (head === null) return (head = node)
+        else {
+            let pointer = head
+            while (pointer.next !== null) {
+                pointer = pointer.next;
+            }
+            pointer.next = node
+            return head
+        }
+    }
+    return {
+        append
+    }
 }
 
 export default LinkedList

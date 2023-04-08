ed991012af813532b4351f597f0d44f388e80a00
Davhho-dev <Davhho@utexas.edu>
2023-04-08 10:51:48 -0500

    Create prepend and size

--- linkedlist.js
+++ linkedlist.js
@@ -1,24 +1,44 @@
 import Node from "../linkedlist/node.js"
 
 function LinkedList() {
-    let head = null;
-    let count = 0;
+    let head = null
+    let count = 0
     
     const append = (value) => {
-        count++;
-        const node = new Node (value);
+        count++
+        const node = new Node (value)
         if (head === null) return (head = node)
         else {
             let pointer = head
             while (pointer.next !== null) {
-                pointer = pointer.next;
+                pointer = pointer.next
             }
             pointer.next = node
+            return node
+        }
+    }
+
+    const prepend = (value) => {
+        count++
+        let temp = head
+        const node = new Node (value)
+        if (head === null) return (head = node)
+        else {
+            let temp = head
+            head = node
+            node.next = temp
             return head
         }
     }
+
+    const size = () => {
+        return count
+    }
+
     return {
-        append
+        append,
+        prepend,
+        size
     }
 }
 
